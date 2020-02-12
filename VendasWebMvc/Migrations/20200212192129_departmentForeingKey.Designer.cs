7ad3e4e410cce14e09ea522571b72982ad46edee
Samuel Sirkis <samuelsirkis@hotmail.com>
2020-02-12 16:27:20 -0300

    Foreign key not null (referential integrity)

--- VendasWebMvc/Migrations/20200212192129_departmentForeingKey.Designer.cs
+++ VendasWebMvc/Migrations/20200212192129_departmentForeingKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VendasWebMvc.Data;
 
 namespace VendasWebMvc.Migrations
 {
     [DbContext(typeof(VendasWebMvcContext))]
-    partial class VendasWebMvcContextModelSnapshot : ModelSnapshot
+    [Migration("20200212192129_departmentForeingKey")]
+    partial class departmentForeingKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
