9c95006e8cb64e4b26e3e9338204a0ab7bdc3068
Samuel Sirkis <samuelsirkis@hotmail.com>
2020-02-12 14:04:34 -0300

    SellerService and basic FindAll

---
+++ VendasWebMvc/Services/SellerService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendasWebMvc.Data;
+using VendasWebMvc.Models;
+
+namespace VendasWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly VendasWebMvcContext _context;
+
+        public SellerService(VendasWebMvcContext context)
+        {
+            _context = context; 
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
--- VendasWebMvc/Startup.cs
+++ VendasWebMvc/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using VendasWebMvc.Data;
+using VendasWebMvc.Services;
 
 namespace VendasWebMvc
 {
@@ -32,6 +33,7 @@ namespace VendasWebMvc
                     builder.MigrationsAssembly("VendasWebMvc")));
 
             services.AddScoped<SeedingServices>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
