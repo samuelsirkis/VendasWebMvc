238555eb6bc2f6893fbd92f8494c5998ddf1de2b
Samuel Sirkis <samuelsirkis@hotmail.com>
2020-02-12 16:02:11 -0300

    Simple Create form

--- VendasWebMvc/Controllers/SellersController.cs
+++ VendasWebMvc/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using VendasWebMvc.Models;
 using VendasWebMvc.Services;
 
 namespace VendasWebMvc.Controllers
@@ -20,6 +21,18 @@ namespace VendasWebMvc.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- VendasWebMvc/Services/SellerService.cs
+++ VendasWebMvc/Services/SellerService.cs
@@ -20,5 +20,11 @@ namespace VendasWebMvc.Services
         {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
